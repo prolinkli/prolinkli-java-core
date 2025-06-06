55f3a7529c872c6d417ff812dabce8595d4d56e8
Kevin Erdogan <identityofsine@gmail.com>
2025-06-05 20:35:24 -0400

    got logger to work

--- src/main/java/com/prolinkli/framework/ProlinkliApplication.java
+++ src/main/java/com/prolinkli/framework/ProlinkliApplication.java
@@ -1,5 +1,6 @@
-package com.prolinkli.core;
+package com.prolinkli.framework;
 
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProlinkliApplication {
 
+	private final static Logger LOGGER =
+			org.slf4j.LoggerFactory.getLogger(ProlinkliApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProlinkliApplication.class, args);
+		LOGGER.info("Prolinkli Application started successfully.");
 	}
 
 }
--- start-dev.sh
+++ start-dev.sh
@@ -4,4 +4,6 @@ source ./bash/log.sh
 
 # This script is used to start the development server for the project.
 
-log "Starting development server..."
+log "Starting development server"
+
+mvn spring-boot:run -Dspring-boot.run.profiles=dev -Dspring-boot.run.jvmArguments="-Xmx1024m -Xms512m"
