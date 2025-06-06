b8999a60c6e3659b85b72735155efe5d06527ad7
Kevin Erdogan <identityofsine@gmail.com>
2025-06-05 20:29:25 -0400

    creating spring application

---
+++ src/main/java/com/prolinkli/core/ProlinkliApplication.java
@@ -0,0 +1,16 @@
+package com.prolinkli.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ProlinkliApplication
+ */
+@SpringBootApplication
+public class ProlinkliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProlinkliApplication.class, args);
+	}
+
+}
