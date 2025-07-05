e61b6a9bdce554d73be499f72bfeb1168840f6a8
kevin <identityofsine@gmail.com>
2025-07-05 13:14:59 -0400

    added event handler

--- src/main/java/com/prolinkli/framework/jwt/listener/JwtExpirationListener.java
+++ src/main/java/com/prolinkli/framework/jwt/listener/JwtExpirationListener.java
@@ -3,20 +3,24 @@ package com.prolinkli.framework.jwt.listener;
 import com.prolinkli.framework.jwt.event.JwtExpirationEvent;
 import com.prolinkli.framework.jwt.model.JWTTokenExpiredException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtExpirationListener implements ApplicationListener<JwtExpirationEvent> {
 
+  private final static Logger LOGGER = LoggerFactory.getLogger(JwtExpirationListener.class);
+
   @Override
   public void onApplicationEvent(JwtExpirationEvent event) {
 
     // Handle the JWT expiration event
     // This could involve logging, notifying users, or performing cleanup actions
-    System.out.println("JWT Token expired for: " + event.getAuthToken().getAccessToken());
     // Additional logic can be added here as needed
-    //
+    LOGGER.warn("JWT Token expired: {}", event.getAuthToken().getAccessToken());
+
   }
 
 }
